a8274905ce8c3cf9f6637b74a29fb397b302cdfd
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2023-11-29 15:35:58 +0200

    Add css styles for tablet and desktop, add js logic to button

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,15 @@
 // HERE PLACE YOUR CODE FOR BURGER MENU
 
-
-
 // HERE PLACE YOUR CODE FOR FORM
 
+// HERE PLACE YOUR EXTRA CODES AND NAME THEM PROPERLY
+let inputEmail = document.querySelector(".email-input");
+let form = document.querySelector("form");
 
-
-// HERE PLACE YOUR EXTRE CODES AND NAME THEM PROPERLY
+form.addEventListener("submit", function () {
+  if (inputEmail) {
+    alert("Pavyko!!!!");
+  } else {
+    alert("Veskite dar karta...");
+  }
+});
